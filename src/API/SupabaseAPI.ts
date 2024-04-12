ae065368a7eff916291125c095192a2d78a7f3bf
positive-season <scp94373@gmail.com>
2024-04-12 14:42:38 +0100

    Removed unnecessary spacing

--- src/API/SupabaseAPI.ts
+++ src/API/SupabaseAPI.ts
@@ -4,4 +4,3 @@ const supabaseURL = "https://kyndzadezcycquyqqwdq.supabase.co";
 const supabaseAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5bmR6YWRlemN5Y3F1eXFxd2RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDY5NzgsImV4cCI6MjAyODQyMjk3OH0.OrvA_keyA4qbt7GDxnPSU85Tj2Q2f-kXBnvt0icdlvg";
 
 export const supabase = createClient(supabaseURL, supabaseAPIKey);
-
